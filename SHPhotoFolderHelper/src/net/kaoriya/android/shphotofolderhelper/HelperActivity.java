1b79c1c3334aaf676df0d9bfb046f0f393c17229
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-08-07 20:26:49 +0900

    import an android application

---
+++ SHPhotoFolderHelper/src/net/kaoriya/android/shphotofolderhelper/HelperActivity.java
@@ -0,0 +1,17 @@
+package net.kaoriya.android.shphotofolderhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelperActivity extends Activity
+{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
