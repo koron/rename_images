169f36cf6acbba479a99ac334dff211896f4cbb2
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-08-07 10:50:21 +0900

    add an exif investigation tool

---
+++ show_exif_times.pl
@@ -0,0 +1,27 @@
+#!/usr/local/bin/perl
+
+use utf8;
+use strict;
+use warnings;
+
+use File::Next;
+use Image::ExifTool;
+
+my $EXIFTOOL = Image::ExifTool->new();
+
+my $files = File::Next::files($ARGV[0]);
+while (defined(my $file = $files->())) {
+    &check_times($file);
+}
+
+sub check_times
+{
+    my ($filename) = @_;
+    print "$filename:\n";
+    my $info = $EXIFTOOL->ImageInfo($filename);
+    foreach my $key (sort keys %$info) {
+        if ($key =~ m/date/i) {
+            print "    $key => $info->{$key}\n";
+        }
+    }
+}
